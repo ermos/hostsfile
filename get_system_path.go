dfaa22a029e7dde06aa2b942e03a3c8cbd17f507
Kilian SMITI <kilian@smiti.fr>
2023-11-11 23:27:55 +0100

    chore: lint

--- get_system_path.go
+++ get_system_path.go
@@ -21,33 +21,26 @@ func getSystemPath(osName string) (path string, err error) {
 	switch osName {
 	case "linux", "darwin", "aix", "dragonfly", "freebsd", "ios", "netbsd", "openbsd", "solaris":
 		path = "/etc/hosts"
-		break
 	case "windows":
 		path = fmt.Sprintf(`%s\System32\drivers\etc\hosts`, os.Getenv("SystemRoot"))
-		break
 	case "android":
 		if _, err = statFile("/system/etc/hosts"); err != nil {
 			path = "/etc/hosts"
 			break
 		}
 		path = "/system/etc/hosts"
-		break
 	case "hurd":
 		if _, err = statFile("/hurd/hosts"); err != nil {
 			path = "/etc/hosts"
 			break
 		}
 		path = "/hurd/hosts"
-		break
 	case "illumos":
 		path = "/etc/inet/hosts"
-		break
 	case "plan9":
 		path = "/lib/ndb/local"
-		break
 	case "zos":
 		path = "SYS1.PARMLIB(HOSTS)"
-		break
 	default:
 		return "", ErrUnsupportedOperatingSystem
 	}
